edda6db44c2dccb4eaed8f0bdcfe9342874ed5e8
Sophie Gill <sophie@makersacademy.com>
2018-06-27 18:06:05 +0100

    step 9: implements send message using mocked client

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,12 +1,22 @@
 class Greeter
   CONFIGURATION = {
-    from: ENV['TWILIO_PHONE_NUMBER']  
+    from: ENV['TWILIO_PHONE_NUMBER']
   }
 
   def initialize(client)
+    @client = client
   end
 
   def message
     "Have a great day!"
+  end
+
+  def send_message(recipient)
+    message_config = {
+      from: CONFIGURATION[:from],
+      to: recipient,
+      body: message
+    }
+    @client.messages.create(message_config)
   end
 end
