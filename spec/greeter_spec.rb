9e37aa95732cb8303de5ff44c6cd244d76482bc6
Sophie Gill <sophie@makersacademy.com>
2018-06-27 17:47:40 +0100

    step 3: failing test for send_message

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,9 +1,18 @@
 require 'greeter'
 
 describe Greeter do
+  let(:message) { double(:message) }
+  
   describe '#message' do
     it 'returns the greeting message' do
       expect(subject.message).to eq 'Have a great day!'
+    end
+  end
+
+  describe '#send_message' do
+    it 'returns a message' do
+      recipient = "+44476532212"
+      expect(subject.send_message(recipient)).to eq message
     end
   end
 end
